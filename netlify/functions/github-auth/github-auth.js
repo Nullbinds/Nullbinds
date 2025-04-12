d4e0f2060163d34d7ddd5bdf3cb56e746ece8f37
Nullbinds <wkwk1337@hotmail.com>
2025-04-12 12:24:01 +0200

    Update github-auth.js

--- netlify/functions/github-auth/github-auth.js
+++ netlify/functions/github-auth/github-auth.js
@@ -3,23 +3,48 @@ const axios = require('axios');
 exports.handler = async (event) => {
   const { code } = event.queryStringParameters;
   
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing authorization code" })
+    };
+  }
+
   try {
     const response = await axios.post('https://github.com/login/oauth/access_token', {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
     }, {
-      headers: { Accept: 'application/json' }
+      headers: { 
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
     });
+
+    if (!response.data.access_token) {
+      console.error('GitHub response:', response.data);
+      throw new Error('No access token received');
+    }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ token: response.data.access_token }),
+      body: JSON.stringify({ token: response.data.access_token })
     };
   } catch (error) {
+    console.error('Full error:', {
+      message: error.message,
+      response: error.response?.data,
+      config: error.config
+    });
+    
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: "Auth failed" }),
+      body: JSON.stringify({ 
+        error: "Auth failed",
+        details: error.message,
+        github_response: error.response?.data
+      })
     };
   }
 };

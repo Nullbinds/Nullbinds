50b5967b7d1ff1e3a3952bac63fa2c894f8f768b
Nullbinds <wkwk1337@hotmail.com>
2025-04-12 12:40:49 +0200

    Create get-auth-config.js

---
+++ netlify/functions/github-auth/get-auth-config.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      client_id: process.env.GITHUB_CLIENT_ID,
+      redirect_uri: 'https://nullbinds.netlify.app/auth' // Must match GitHub OAuth app settings
+    })
+  };
+};

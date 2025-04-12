cd4790bee16b1579c7bf69d868d7cd2f2522aec8
Nullbinds <wkwk1337@hotmail.com>
2025-04-12 13:22:46 +0200

    Update get-auth-url.js

--- netlify/functions/get-auth-url.js
+++ netlify/functions/get-auth-url.js
@@ -1,9 +1,19 @@
-// netlify/functions/get-auth-url.js
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      url: `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent('https://nullbinds.netlify.app/')}&scope=repo`
-    })
-  };
+  try {
+    if (!process.env.GITHUB_CLIENT_ID) {
+      throw new Error('Missing client ID');
+    }
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        url: `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent('https://nullbinds.netlify.app/')}&scope=repo`
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
 };

01969d8dff65aab0cdbbaaf544b2f119a94e2f64
Nullbinds <wkwk1337@hotmail.com>
2025-04-12 12:11:27 +0200

    Create github-auth.js

---
+++ netlify/functions/github-auth/github-auth.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  const { code } = event.queryStringParameters;
+  
+  try {
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }, {
+      headers: { Accept: 'application/json' }
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ token: response.data.access_token }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Auth failed" }),
+    };
+  }
+};
